04c4d4a22fb4b62c1878e4b4ac1a837cdef6d861
Sam <s2chowdhury@ryerson.ca>
2020-07-28 02:27:07 -0400

    Minor changes

---
+++ Assets/Scripts/IInteractable.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+namespace enemy
+{
+    public interface IInteractable{
+
+        // {get;} is c# syntax for getters 
+        float holdDuration { get; }
+        bool holdInteractable { get; }
+        bool multipleUse { get; }
+        bool isInteractable { get; }
+        float maxRange { get; }
+
+        void OnStartHover();
+        void OnInteract();
+        void OnEndHover();
+    }
+}
